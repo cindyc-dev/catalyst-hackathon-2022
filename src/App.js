f14d0726f1392767dafaa0d85dd0bac2494f5036
Xin Yu <chuahxinyu2@gmail.com>
2022-08-27 12:31:39 +1000

    Added Tabs

--- src/App.js
+++ src/App.js
@@ -2,8 +2,48 @@ import './App.css';
 
 import React from 'react';
 import ImageUploading from 'react-images-uploading';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 function App() {
+  const [tabVal, setTabVal] = React.useState(0);
+
   const [images, setImages] = React.useState([]);
   const maxNumber = 69;
 
@@ -12,9 +52,27 @@ function App() {
     console.log(imageList, addUpdateIndex);
     setImages(imageList);
   };
+  
 
   return (
     <div className="App">
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={tabVal} onChange={(e, newVal) => setTabVal(newVal)} aria-label="basic tabs example">
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={tabVal} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={tabVal} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={tabVal} index={2}>
+        Item Three
+      </TabPanel>
+
       <ImageUploading
         multiple
         value={images}
@@ -54,6 +112,7 @@ function App() {
           </div>
         )}
       </ImageUploading>
+
     </div>
   );
 }
