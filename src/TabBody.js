1ef5d8501f266dd0bf1e9dd0604aee9c7e59673f
Xin Yu <chuahxinyu2@gmail.com>
2022-08-28 12:31:49 +1000

    Added testing buttons for endpoint

--- src/TabBody.js
+++ src/TabBody.js
@@ -1,29 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageCarousel from "./components/ImageCarousel";
 import { Box, Grid, TextField, Button } from "@mui/material";
+import { getPages, publishPost, uploadPhoto } from "./api/facebook";
 
 const TabBody = ({ images }) => {
-  return (
-    <Box width={1000} mx={"auto"}>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <ImageCarousel images={images} />
-        </Grid>
-        <Grid style={{height: "100%"}} item xs={6} sx={{textAlign: 'left'}}>
-          <Grid><TextField
-            label="Description"
-            multiline
-            rows={12}
-            sx={{ width: "100%" }}
-          /> 
-          </Grid>
-          <Grid style={{height: "100%"}}>
-          <Button>click to post</Button>
-        </Grid>
-        </Grid>
-      </Grid>
-    </Box>
-  )
-}
+    const [description, setDescription] = useState("");
+    return (
+        <Box width={1000} mx={"auto"}>
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <ImageCarousel images={images} />
+                </Grid>
+                <Grid
+                    style={{ height: "100%" }}
+                    item
+                    xs={6}
+                    sx={{ textAlign: "left" }}
+                >
+                    <Grid>
+                        <TextField
+                            label="Description"
+                            multiline
+                            rows={12}
+                            sx={{ width: "100%" }}
+                            value={description}
+                            onChange={(e) => setDescription(e.currentTarget.value)}
+                        />
+                    </Grid>
+                    <Grid style={{ height: "100%" }}>
+                    <Button
+                          onClick={() => {
+                            getPages(localStorage.getItem("facebook"))
+                          }}
+                        >getpages</Button>
+                        <Button
+                          onClick={() => {
+                            publishPost()
+                          }}
+                        >publish post</Button>
+                        <Button
+                            onClick={() => {
+                                console.log(localStorage.getItem("facebook"));
+                                uploadPhoto(
+                                    "me",
+                                    localStorage.getItem("facebook"),
+                                    images[0]["data_url"]
+                                );
+                            }}
+                        >
+                            test fb uplaod
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
 
 export default TabBody;
--- src/api/facebook.js
+++ src/api/facebook.js
@@ -1,14 +1,24 @@
 import axios from "axios";
 
+export const getUserId = async (accessToken) => {
+  const res = await axios.get(`https://graph.facebook.com/me?access_token=${accessToken}`)
+  console.log(`userId: ${JSON.stringify(res, null, 2)}`)
+  return res.data.id
+}
+
 /**
  * Retreive all the pages that the user has access to.
  * @param {string} userId id of user from login
  * @param {string} accessToken user access token from login
  * @returns {Promise} promise that resolves to an array of pages, containing `name`, `access_token`, and `id` of each page
  */
-export const getPages = (userId, accessToken) => {
-  axios.get(`https://graph.facebook.com/${userId}/accounts?fields=name,access_token&access_token=${accessToken}`)
-    .then(res => res.data)
+export const getPages = async (accessToken) => {
+  const userId = await getUserId(accessToken)
+  return await axios.get(`https://graph.facebook.com/${userId}/accounts?fields=name,access_token&access_token=${accessToken}`)
+    .then(res => {
+      console.log(JSON.stringify(res, null, 2))
+      return res.data
+    })
     .catch(err => err);
 }
 
@@ -20,6 +30,7 @@ export const getPages = (userId, accessToken) => {
  */
 export const publishPost = (pageId, accessToken, content) => {
   let message = "";
+  let args = {}
 
   if (content.message) {
     message = "message=" + content.message;
@@ -30,12 +41,10 @@ export const publishPost = (pageId, accessToken, content) => {
   }
 
   if (content.photo) {
-    message += "&photo=" + content.photo;
+    args.object_attachment = content.photo
   }
 
-  axios.post(`https://graph.facebook.com/${pageId}/feed?${message}&access_token=${accessToken}`, {
-    object_attachment: content.photo
-  })
+  axios.post(`https://graph.facebook.com/${pageId}/feed?${message}&access_token=${accessToken}`, args)
     .then(res => res.data)
     .catch(err => err);
 }
