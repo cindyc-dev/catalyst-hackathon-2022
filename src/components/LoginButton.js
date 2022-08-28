bc25904fa81c30a622f76adfbd5135b009a0f41f
Xin Yu <chuahxinyu2@gmail.com>
2022-08-28 12:32:49 +1000

    Add name to Login; Add fb permission
    cannot continue with fb because we need an App Review to manage pages

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Button } from "@mui/material";
 import LoginIcon from '@mui/icons-material/Login';
 
-function LoginButton({ handleClick, icon=<LoginIcon /> }) {
+function LoginButton({ name, handleClick, icon=<LoginIcon /> }) {
   return (
     <Button
         variant="contained"
         onClick={handleClick}
         startIcon={icon}
     >
-        Login
+        {name} Login
     </Button>
   )
 }
